3625d5563cde8988ad4f50e1c7aeb4fda63f09bd
schochastics <david@schochastics.net>
2024-02-18 19:12:21 +0100

    removed many dependencies

--- import.R
+++ import.R
@@ -1,24 +1,24 @@
 library(gh)
-library(tibble)
-repos <- readr::read_csv("repos.csv")
+
+repos <- read.csv("repos.csv", row.names = NULL)
 get_repo <- function(owner, repo) {
     res_issues <- gh("/repos/{username}/{repo}/issues", username = owner, repo = repo)
     res_repo <- gh("/repos/{username}/{repo}", username = owner, repo = repo)
-    tibble(
+    data.frame(
         owner = owner,
         repo = repo,
         last_push = lubridate::as_datetime(res_repo[["pushed_at"]]),
         stars = res_repo[["stargazers_count"]],
         forks = res_repo[["forks"]],
         open_issues = res_repo[["open_issues_count"]],
-        last_issue_activity = suppressWarnings(lubridate::as_datetime(max(purrr::map_chr(res_issues, \(x) x[["updated_at"]])))),
+        last_issue_activity = suppressWarnings(lubridate::as_datetime(max(purrr::map_chr(res_issues, \(x) x[["updated_at"]]))))
     )
 }
 
-repos <- readr::read_csv("repos.csv")
-res <- purrr::map_dfr(seq_len(nrow(repos)), \(x){
+lst <- lapply(seq_len(nrow(repos)), \(x) {
     get_repo(repos$owner[x], repos$repo[x])
 })
 
-readr::write_csv(readr::read_csv("current_week.csv"), "last_week.csv")
-readr::write_csv(res, "current_week.csv")
+res <- do.call("rbind", lst)
+write.csv(read.csv("current_week.csv", row.names = NULL), "last_week.csv", row.names = FALSE)
+write.csv(res, "current_week.csv", row.names = FALSE)
